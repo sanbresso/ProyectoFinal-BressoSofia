17f6fc20bad88914127f32be857528b4a7dc6799
Sofia Bresso <sofibressapps@gmail.com>
2024-01-22 23:21:22 -0300

    agregado de files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var body = document.body;
+    body.style.backgroundColor = "#000000";
+});
+
+const productos = {
+    "top-1": {
+        nombre: "CORSÉ BACK",
+        precio: 425,
+        imagen: "../img/top-cat-1.webp"
+    },
+    "top-2": {
+        nombre: "CORSÉ COPA",
+        precio: 425,
+        imagen: "../img/top-cat-2.webp"
+    }
+};
+
